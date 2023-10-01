1cfce683304964160a365f39214ea81c5426bc7d
Mello Birkan <mgbirkan@icloud.com>
2023-10-01 20:12:19 -0300

    fazendo o primeiro server `http`

---
+++ L4. Building for the Web/1. Intro to Handlers/exe.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var cities = []string{"Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City"}
+
+func index2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<a href=/cities>Hello stranger<a>")
+}
+
+func cityList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "List of most populous cities:")
+	for _, city := range cities {
+		fmt.Fprintln(w, city)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index2)
+	http.HandleFunc("/cities", cityList)
+	fmt.Println("Server started!")
+	http.ListenAndServe(":3000", nil)
+}
