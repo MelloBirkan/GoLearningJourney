278082c93a3d9c34f40512ef5f02b4227fd564f4
Mello Birkan <mgbirkan@icloud.com>
2023-10-02 01:07:50 -0300

    Conteudo sobre `HTML` com GO

--- L4. Building for the Web/4. Serving HTML/main.go
+++ L4. Building for the Web/4. Serving HTML/main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+func showContactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./L4. Building for the Web/4. Serving HTML/static/index.html")
+}
+
 func main() {
+	http.HandleFunc("/", showContactPage)
 	// "FileServer returns a handler that serves HTTP requests with the contents of the file system rooted at root"
 	// See: https://pkg.go.dev/net/http#FileServer
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
+
+	// Tambem Funciona
+	//fileServer := http.FileServer(http.Dir("./static"))
+	//http.Handle("/", fileServer)
 
 	fmt.Println("Server is starting...")
 	http.ListenAndServe(":3000", nil)
